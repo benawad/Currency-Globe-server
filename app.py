b830d765ad1ed910f3f1241ac63cc2ccfd841a01
ben awad <benawad97@gmail.com>
2016-10-26 16:55:45 -0500

    Serve static data

--- app.py
+++ app.py
@@ -1,5 +1,32 @@
-from flask import Flask
+from flask import Flask, g
+from pymongo import MongoClient
+import os
+import json
+
 app = Flask(__name__)
+
+
+@app.route("/data")
+def data():
+    client = get_db()
+    db = client.get_default_database()
+    return json.dumps(db.data.find_one()['data'])
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'mongo_db'):
+        g.mongo_db = MongoClient(os.environ["MONGO_URI"])
+    return g.mongo_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'mongo_db'):
+        g.mongo_db.close()
 
 
 @app.route('/')
